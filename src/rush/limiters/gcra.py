6e20cf6d5328ec14024c9d6e3a5e6308f813c1fe
Ian Stapleton Cordasco <graffatcolmingov@gmail.com>
2018-12-21 14:09:40 -0600

    Add our Generic Cell Ratelimiting Algorithm
    This completes the minimum I wanted for the project as far as code is
    concerned. Next to add documentation.

---
+++ src/rush/limiters/gcra.py
@@ -0,0 +1,99 @@
+"""Module containing implementations for GCRA."""
+import datetime
+import math
+
+from . import base
+from .. import limit_data
+from .. import quota
+from .. import result
+
+
+class GenericCellRatelimiter(base.BaseLimiter):
+    """A Generic Cell Ratelimit Algorithm implementation in Pure Python."""
+
+    def rate_limit(
+        self, key: str, quantity: int, rate: quota.Quota
+    ) -> result.RateLimitResult:
+        """Apply the rate-limit to a quantity of requests."""
+        # Emission interval is how much is allowed per period
+        emission_interval: datetime.timedelta = rate.period / rate.limit
+        limit = rate.limit
+        delay_variation_tolerance: datetime.timedelta = (
+            emission_interval * limit
+        )
+        # The increment uses the emission interval to find out how much time
+        # quantity should have been issued over
+        increment: datetime.timedelta = emission_interval * quantity
+        now = datetime.datetime.now(datetime.timezone.utc)
+        data = self.store.get(key)
+        # tat is short for theoretical arrival time, we store this as
+        # "time" on our limit data
+        tat = getattr(data, "time", None) or now
+        new_tat = max(now, tat) + increment
+        # The theoretical arrival time defines the end-period (in the future)
+        # of our bucket. We want to find, however, which is later - our tat or
+        # now. Once we have that, we can find the earliest point in our bucket
+        # which is the time we would start allowing new requests.
+        allow_at: datetime.datetime = new_tat - delay_variation_tolerance
+        # Okay, we have our tat, the time we would have started allowing new
+        # requests, and present time so let's figure out the difference from
+        # our earliest point in time and now.
+        distance_from_start_of_bucket = now - allow_at
+        # Now that we know how far we are from the start, let's find out how
+        # much we have remaining in our quota.
+        remaining = math.floor(
+            (distance_from_start_of_bucket / emission_interval) + 0.5
+        )
+        # We also need to calculate the next reset_after for the user
+        reset_after = tat - now
+        if reset_after.total_seconds() == 0:
+            reset_after = datetime.timedelta(seconds=-1)
+
+        if remaining < 1:
+            # It's possible that distance_from_start_of_bucket is negative so
+            # it is then also possible that there are fewer than 0 remaining
+            # requests available. In that case, we're going to be ratelimited.
+            remaining = 0
+            limited = True
+            retry_after = emission_interval - distance_from_start_of_bucket
+            new_time = tat
+        else:
+            limited = False
+            retry_after = datetime.timedelta(seconds=-1)
+            new_time = new_tat
+
+        used = rate.limit - remaining
+        if data is not None:
+            limitdata = data.copy_with(
+                used=used, remaining=remaining, time=new_time
+            )
+        else:
+            limitdata = limit_data.LimitData(
+                used=used, remaining=remaining, created_at=now, time=new_time
+            )
+
+        self.store.set(key=key, data=limitdata)
+
+        return result.RateLimitResult(
+            limit=rate.count,
+            limited=limited,
+            remaining=remaining,
+            reset_after=reset_after,
+            retry_after=retry_after,
+        )
+
+    def reset(self, key: str, rate: quota.Quota) -> result.RateLimitResult:
+        """Reset the rate-limit for a given key."""
+        now = datetime.datetime.now(datetime.timezone.utc)
+        reset_tat = now - (rate.period * 2)
+        data = limit_data.LimitData(
+            used=0, remaining=rate.limit, created_at=now, time=reset_tat
+        )
+        self.store.set(key=key, data=data)
+        return result.RateLimitResult(
+            limit=rate.count,
+            limited=False,
+            remaining=rate.limit,
+            reset_after=datetime.timedelta(seconds=-1),
+            retry_after=datetime.timedelta(seconds=-1),
+        )
---
+++ test/unit/test_gcra_limiter.py
@@ -0,0 +1,114 @@
+"""Tests for our fancy Generic Cell Ratelimiter."""
+import datetime
+
+import pytest
+
+from rush import limit_data
+from rush.limiters import gcra
+
+from . import helpers  # noqa: I202
+
+
+@pytest.fixture
+def limiter():
+    """Provide instantiated periodic limiter."""
+    store = helpers.MockStore()
+    return gcra.GenericCellRatelimiter(store=store)
+
+
+class TestGenericCellRatelimiter:
+    """Tests that exercise our GCRA implementation."""
+
+    def test_reset(self, limiter):
+        """Verify we reset our GCRA limiter properly."""
+        rate = helpers.new_quota()
+        mockstore = limiter.store.recording_store
+
+        limitresult = limiter.reset(key="key", rate=rate)
+
+        _, kwargs = mockstore.set.call_args
+        limit_data = kwargs["data"]
+        assert kwargs["key"] == "key"
+        assert (limit_data.created_at - limit_data.time) == (2 * rate.period)
+        assert limitresult.remaining == 5
+        assert limitresult.limit == 5
+        assert limitresult.limited is False
+        assert limitresult.reset_after == datetime.timedelta(seconds=-1)
+        assert limitresult.retry_after == datetime.timedelta(seconds=-1)
+
+    def test_ratelimit_first_time_seeing_key(self, limiter):
+        """Verify our behaviour for the first time we see a key."""
+        rate = helpers.new_quota(
+            period=datetime.timedelta(seconds=60), count=50
+        )
+        mockstore = limiter.store.recording_store
+        mockstore.get.return_value = None
+
+        limitresult = limiter.rate_limit(key="key", rate=rate, quantity=1)
+
+        assert limitresult.limited is False
+        assert limitresult.remaining == 49
+        assert limitresult.reset_after == datetime.timedelta(seconds=-1)
+        assert limitresult.retry_after == datetime.timedelta(seconds=-1)
+
+    def test_ratelimit_existing_key_within_cell(self, limiter):
+        """Verify that if we're inside our cell, we reduce remaining."""
+        rate = helpers.new_quota(
+            period=datetime.timedelta(seconds=60), count=50
+        )
+        mockstore = limiter.store.recording_store
+        mockstore.get.side_effect = lambda key: (
+            limit_data.LimitData(
+                used=1,
+                remaining=49,
+                created_at=datetime.datetime.now(datetime.timezone.utc),
+                time=(
+                    datetime.datetime.now(datetime.timezone.utc)
+                    + datetime.timedelta(seconds=1)
+                ),
+            )
+        )
+
+        limitresult = limiter.rate_limit(key="key", rate=rate, quantity=1)
+
+        assert limitresult.limited is False
+        assert limitresult.remaining == 48
+        assert (
+            datetime.timedelta(seconds=0)
+            < limitresult.reset_after
+            < (datetime.timedelta(seconds=60) / 50)
+        )
+        assert limitresult.retry_after == datetime.timedelta(seconds=-1)
+
+    def test_ratelimit_exceeded(self, limiter):
+        """Verify that if we're inside our cell, we reduce remaining."""
+        rate = helpers.new_quota(
+            period=datetime.timedelta(seconds=60), count=50
+        )
+        mockstore = limiter.store.recording_store
+        mockstore.get.side_effect = lambda key: (
+            limit_data.LimitData(
+                used=49,
+                remaining=1,
+                created_at=datetime.datetime.now(datetime.timezone.utc),
+                time=(
+                    datetime.datetime.now(datetime.timezone.utc)
+                    + datetime.timedelta(seconds=60)
+                ),
+            )
+        )
+
+        limitresult = limiter.rate_limit(key="key", rate=rate, quantity=1)
+
+        assert limitresult.limited is True
+        assert limitresult.remaining == 0
+        assert (
+            datetime.timedelta(seconds=60)
+            <= limitresult.reset_after
+            < (2 * datetime.timedelta(seconds=60))
+        )
+        assert (
+            datetime.timedelta(seconds=0)
+            < limitresult.retry_after
+            <= datetime.timedelta(seconds=3)
+        )
