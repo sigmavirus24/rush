149c57b84f8b7c29347bbf59a2e88b0f64247cb3
Ian Stapleton Cordasco <graffatcolmingov@gmail.com>
2018-12-22 10:27:31 -0600

    Update our sphinx config to pull versions from source
    This should prevent our sphinx version and package version from
    diverging.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,10 +23,12 @@ project = 'rush'
 copyright = '2018, Ian Stapleton Cordasco'
 author = 'Ian Stapleton Cordasco'
 
+import rush
+
 # The short X.Y version
-version = ''
+version = rush.__version__
 # The full version, including alpha/beta/rc tags
-release = '2018.12.0'
+release = rush.__version__
 
 
 # -- General configuration ---------------------------------------------------
@@ -78,7 +80,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
